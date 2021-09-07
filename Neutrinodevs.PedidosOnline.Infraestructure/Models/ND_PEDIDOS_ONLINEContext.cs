99f991feefb58b00837ff55a978d6e998908c9a0
Byron Duarte <byronduarte95@gmail.com>
2021-09-07 00:19:17 -0500

    avance en validaciones básicas formulario de registro/login

--- Neutrinodevs.PedidosOnline.Infraestructure/Models/ND_PEDIDOS_ONLINEContext.cs
+++ Neutrinodevs.PedidosOnline.Infraestructure/Models/ND_PEDIDOS_ONLINEContext.cs
@@ -28,7 +28,6 @@ namespace Neutrinodevs.PedidosOnline.Infraestructure.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Data Source=BDUARTE-LAP; Initial Catalog=ND_PEDIDOS_ONLINE; user id=sa; password=12345678");
             }
         }
---
+++ Neutrinodevs.PedidosOnline.Presentation/wwwroot/js/order/checkout.js
@@ -0,0 +1,44 @@
+﻿$("#frmRegistrarme").submit(function (e) {
+    e.preventDefault();
+
+    let user = {
+        identification: document.querySelector("#identification"),
+        first_name: document.querySelector("#name"),
+        last_name: document.querySelector("#last_name"),
+        email: document.querySelector("#email"),
+        pass: document.querySelector("#password"),
+        pass2: document.querySelector("#passsword2")
+    }
+
+
+    let form_valid = true;
+    Object.entries(user).forEach(([clave, valor]) => {
+
+        if (valor.value.length == 0) {
+            valor.classList.add('invalid');
+            if (form_valid) form_valid = false;
+        } else {
+            valor.classList.remove('invalid');
+        }
+
+
+    });
+
+
+    if (user.pass != user.pass2) {
+        if ($("#password").next('span').length < 1) {
+            var message = document.createElement("span");
+            message.innerHTML = "Las contraseñas no son iguales.";
+            message.classList.add("color-primary", "ml-2", "font-weight");
+
+            let pass = document.querySelector("#password");
+            pass.after(message);
+        }
+    } else {
+        $("#password").next("span").remove();
+    }
+
+    if (!form_valid) return;
+
+    //llamada ajax
+});

10e3b172e2d7564c252c8a8c13253e03b3bbf72e
Byron Duarte <byronduarte95@gmail.com>
2021-11-05 22:36:14 -0500

    Validaciones en checkout

--- Neutrinodevs.PedidosOnline.Presentation/wwwroot/js/main.js
+++ Neutrinodevs.PedidosOnline.Presentation/wwwroot/js/main.js
@@ -1,1 +1,27 @@
-﻿
+﻿$(document).ready(function () {
+
+    $('.numbers').keypress(function (e) {
+        tecla = (document.all) ? e.keyCode : e.which;
+        if (tecla == 8) return true;
+        patron = /[\d]/;
+        te = String.fromCharCode(tecla);
+        return patron.test(te);
+    });
+
+    $('.letters').keypress(function (e) {
+        tecla = (document.all) ? e.keyCode : e.which;
+        if (tecla == 8) return true;
+        patron = /[A-Za-zñÑ\s]/;
+        te = String.fromCharCode(tecla);
+        return patron.test(te);
+    });
+
+    $('.alphanumeric').keypress(function (e) {
+        tecla = (document.all) ? e.keyCode : e.which;
+        if (tecla == 8) return true;
+        patron = /[0-9A-Za-zñÑ\s]/;
+        te = String.fromCharCode(tecla);
+        return patron.test(te);
+    });
+
+});

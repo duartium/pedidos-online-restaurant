509481060bb37abaf662e8a3a4057c688a48b898
Byron Duarte <byronduarte95@gmail.com>
2021-11-29 21:11:33 -0500

    Se avanza con el menu responsive para home

--- Neutrinodevs.PedidosOnline.Presentation/wwwroot/js/main.js
+++ Neutrinodevs.PedidosOnline.Presentation/wwwroot/js/main.js
@@ -24,4 +24,12 @@
         return patron.test(te);
     });
 
+    const btn_menu = document.querySelector(".btn-menu");
+    if (btn_menu) {
+        btn_menu.addEventListener("click", () => {
+            const menu_items = document.querySelector(".menu-items");
+            menu_items.classList.toggle('show');
+        });
+    }
+
 });

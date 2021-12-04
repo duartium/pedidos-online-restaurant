dc9805d201dc0d92f5a89e0d43f4aa04f81d97d2
Byron Duarte <byronduarte95@gmail.com>
2021-12-04 11:55:08 -0500

    se permite eliminar productos del carrito

--- Neutrinodevs.PedidosOnline.Presentation/wwwroot/js/order/shopping-cart.js
+++ Neutrinodevs.PedidosOnline.Presentation/wwwroot/js/order/shopping-cart.js
@@ -3,6 +3,15 @@
     overlay: true
 });
 
+var iterator_prod;
+if (localStorage.getItem('iterator_prod')) {
+    iterator_prod = parseInt(localStorage.getItem('iterator_prod'));
+}
+else {
+    iterator_prod = 0;
+    localStorage.setItem('iterator_prod', iterator_prod.toString());
+}
+
 $(document).on("click", ".card", function () {
     $("#txt_quantity").val('1');
     let dish = $(this).data('dish');
@@ -15,8 +24,9 @@ $(document).on("click", ".card", function () {
 
 
 $("#add-cart").click(function () {
-
+    iterator_prod++;
     var product_order = {
+        uid: iterator_prod,
         id: parseInt($("#product-modal").data('id')),
         name: $("#product-title").text(),
         price: $("#modal-price").text(),
@@ -27,6 +37,7 @@ $("#add-cart").click(function () {
     $("#quantity_cart").text(quantity_cart.toString());
 
     saveLocalStorage(product_order);
+    localStorage.setItem('iterator_prod', iterator_prod.toString());
 
     $("#txt_quantity").val('1');
     $("#product-modal").modal('hide');
@@ -68,19 +79,36 @@ const setPendingOrder = () => {
     }
 }
 
+const removeProdFromCart = (id_item, element, num_products) => {
+    let order = localStorage.getItem('order');
+    let items = JSON.parse(order);
+    console.log(items);
+    let new_items = items.filter(x => x.uid != id_item);
+    console.log(new_items);
+
+    let quantity_cart = parseInt($("#quantity_cart").text());
+    quantity_cart -= num_products;
+    $("#quantity_cart").text(quantity_cart.toString());
+
+    localStorage.setItem("order", JSON.stringify(new_items));
+    element.parentNode.parentNode.removeChild(element.parentNode);
+}
+
 const setElementsToCart = (items) => {
     let wrap_items = $(".order-items");
     let subtotal = 0;
     let content = '';
     items.forEach(x => {
         subtotal += parseFloat(x.price);
-        content += `<li><a class="bg-danger text-white">×</a><span class="title-item-cart">${x.name} (${x.quantity})</span><br />`;
+        content += `<li><a class="bg-danger text-white" onclick="removeProdFromCart(${x.uid}, this, ${x.quantity})">×</a><span class="title-item-cart">${x.name} (${x.quantity})</span><br />`;
         content += `<span class="price-item-cart">$${x.price}</span></li>`;
     });
     wrap_items.html(content);
     $("#subtotal-cart").text(subtotal.toFixed(2));
     disabledCartButtons(false);
 }
+
+
 
 const disabledCartButtons = (enabled) => {
     if (enabled) {
@@ -115,6 +143,8 @@ $("#btn_empty_cart").click(function () {
     }).then((result) => {
         if (result.isConfirmed) {
             localStorage.removeItem("order");
+            iterator_prod = 0;
+            localStorage.removeItem("iterator_prod");
             $(".order-items").html('');
             $("#subtotal-cart").text(parseFloat(0).toFixed(2));
             $("#quantity_cart").text(0);
